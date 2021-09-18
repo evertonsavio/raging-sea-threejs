f2b4462ffdf32e9ad2eb0ba815b63976fb761549
Rhavyx <everluca@outlook.com>
2021-09-18 20:24:09 -0300

    :sparkles: wave animation

--- src/script.js
+++ src/script.js
@@ -29,6 +29,7 @@ const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
     fragmentShader: waterFragmentShader,
     uniforms: {
+        uTime: { value: 0},
         uBigWavesElevation: { value: 0.2 },
         uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5)}
     }
@@ -96,6 +97,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update water
+    waterMaterial.uniforms.uTime.value = elapsedTime;
 
     // Update controls
     controls.update()
